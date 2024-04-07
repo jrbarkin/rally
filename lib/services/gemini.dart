e0a52be203b1f8240265a29572ede2283736553e
jrbarkin <jrbarkin1@gmail.com>
2024-04-06 17:11:36 -1000

    added param and return type

--- lib/services/gemini.dart
+++ lib/services/gemini.dart
@@ -3,10 +3,9 @@ import 'dart:io';
 import 'package:google_generative_ai/google_generative_ai.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-void get_query() async {
+Future getQuery(String query) async {
   // Access your API key as an environment variable (see "Set up your API key" above)
   final apiKey = dotenv.env['API_KEY'];
-  print(apiKey);
   if (apiKey == null) {
     print('No \$API_KEY environment variable');
     exit(1);
@@ -14,7 +13,12 @@ void get_query() async {
   // For text-only input, use the gemini-pro model
   final model = GenerativeModel(model: 'gemini-pro', apiKey: apiKey);
   final location = 'New York';
-  final content = [Content.text('Get me events in $location this weekend. Respond textually, then include raw JSON specifying the event name, time, and street address.')];
+  final content = [Content.text(query + 'Get me events in $location this weekend. Respond textually, then include raw JSON specifying the event name, time, and street address.')];
   final response = await model.generateContent(content);
   print(response.text);
+
+  // find json in response.text and convert it to JSON variable
+
+  return response.text;
+
 }
