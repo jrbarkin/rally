7b459df1f6d2a35305e22fee1f7e4e1d467c0c4e
jrbarkin <jrbarkin1@gmail.com>
2024-04-07 23:20:22 -1000

    Removed MyApp 'const' attribute

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:rally/pages/map_screen.dart';
-import 'services/gemini.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+
 
 // DotEnv dotenv = DotEnv() is automatically called during import.
 // If you want to load multiple dotenv files or name your dotenv object differently, you can do the following and import the singleton into the relavant files:
