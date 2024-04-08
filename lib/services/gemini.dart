92ce87f9a3c170c46f47b1d7b1b6c6cf7a16f649
jrbarkin <jrbarkin1@gmail.com>
2024-04-07 23:20:43 -1000

    added gemini result class

--- lib/services/gemini.dart
+++ lib/services/gemini.dart
@@ -2,8 +2,25 @@ import 'dart:io';
 
 import 'package:google_generative_ai/google_generative_ai.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'dart:convert';
 
-Future getQuery(String query) async {
+class GeminiResult {
+  final String name;
+  final String address;
+
+  GeminiResult(this.name, this.address);
+
+  GeminiResult.fromJson(Map<String, dynamic> json)
+      : name = json['name'] as String,
+        address = json['address'] as String;
+
+  Map<String, dynamic> toJson() => {
+        'name': name,
+        'email': address,
+      };
+}
+
+Future<List<GeminiResult>> getQuery(String query) async {
   // Access your API key as an environment variable (see "Set up your API key" above)
   final apiKey = dotenv.env['API_KEY'];
   if (apiKey == null) {
@@ -11,14 +28,30 @@ Future getQuery(String query) async {
     exit(1);
   }
   // For text-only input, use the gemini-pro model
-  final model = GenerativeModel(model: 'gemini-pro', apiKey: apiKey);
-  final location = 'New York';
-  final content = [Content.text(query + 'Get me events in $location this weekend. Respond textually, then include raw JSON specifying the event name, time, and street address.')];
+  final model = GenerativeModel(model: 'gemini-1.0-pro', apiKey: apiKey);
+  // create const to ask for  JSON from gemini
+  const jsonRequest = '. Respond via JSON, specifying the location name and street address in the form of a JSON object. For example, {"name": "Crazy Tacos", "address": "123 Main St., San Francisco, CA 94111"}';
+  final content = [Content.text(query + jsonRequest)];
   final response = await model.generateContent(content);
-  print(response.text);
+  final responseText = response.text;
+  print("Response: $responseText");
 
-  // find json in response.text and convert it to JSON variable
+  RegExp exp = RegExp(r"```(.*?)```", dotAll: true);
+  Iterable<RegExpMatch> matches = exp.allMatches(responseText!);
 
-  return response.text;
+  // cast matches to string
+  String originalString = matches.first.group(1).toString();
+  print("Original String: $originalString");
+
+  String formattedString = originalString.replaceFirst(RegExp('JSON|json'), '');
+  print("Formatted String: $formattedString");
+
+    // Parse the JSON string
+  var jsonList = jsonDecode(formattedString) as List;
+
+  // Create a list of GeminiResult objects
+  List<GeminiResult> geminiResultsList = jsonList.map((item) => GeminiResult.fromJson(item)).toList();
+
+  return geminiResultsList;
 
 }
